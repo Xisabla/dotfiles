177d4fbee8046e8c1a18d7c49b90349456b2af22
Gautier Miquet <xisabla.dev@gmail.com>
2021-10-22 10:29:50 +0200

    add jetbrains clion settings

---
+++ jetbrains/clion/fileTemplates/includes/C File Header.h
@@ -0,0 +1,10 @@
+/*=========================================================================
+
+  Project:   ${PROJECT_NAME}
+  File:      ${FILE_NAME}
+
+  Copyright (c) $YEAR - All rights reserved
+  Distributed under the MIT License (https://opensource.org/licenses/MIT)
+
+=========================================================================*/
+
